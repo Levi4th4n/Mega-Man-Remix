84e7c6222e434480885e8ac9180aa02448aded23
Levi <cthompson@hackucf.org>
2023-02-12 21:27:11 -0500

    Added decay to projectiles

--- Mega Man Remix/Assets/Scripts/ProjectileController.cs
+++ Mega Man Remix/Assets/Scripts/ProjectileController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     public float speed = 15.0f;
+    public float timeDelay = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+        Destroy(gameObject, timeDelay);
     }
 
     void OnCollisionEnter2D(Collision2D collision)

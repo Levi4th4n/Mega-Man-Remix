743d2b5acb4b4a3bc501c25d37c546eae566dce6
Levi <cthompson@hackucf.org>
2023-02-12 21:44:49 -0500

    Fixed Camera

--- Mega Man Remix/Assets/Scripts/PlayerController.cs
+++ Mega Man Remix/Assets/Scripts/PlayerController.cs
@@ -77,7 +77,7 @@ public class PlayerController : MonoBehaviour
 
         if (mainCamera)
         {
-            mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+            mainCamera.transform.position = new Vector3(t.position.x, t.position.y, cameraPos.z);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && facingRight)
